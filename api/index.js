b4a45adfd7d1cca8d5621c8c044061635ed8919a
Julian Cancelo <juliancancelo@gmail.com>
2025-11-11 11:43:33 -0300

    Fix: Configurar rewrites para API serverless

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
 // Vercel Serverless Function - API Entry Point
 const app = require('../backend/src/server');
 
-module.exports = app;
+// Exportar como función serverless de Vercel
+module.exports = (req, res) => {
+  // Vercel maneja las rutas, pasamos la request a Express
+  return app(req, res);
+};
